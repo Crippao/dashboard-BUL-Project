ae88816354af3a7f9d226f44b979ea0f0f49661d
Cristina <109718308+Crippao@users.noreply.github.com>
2024-06-25 10:27:42 +0200

    change base url

--- src/server/modules/config.py
+++ src/server/modules/config.py
@@ -1,19 +1,15 @@
 import os
 
-# Ottiengo il percorso assoluto della directory principale del progetto
-project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../"))
 default_data = os.path.join(os.path.dirname(__file__), "../data")
 data_root = os.path.abspath(os.environ.get("DATA_FOLDER", default_data))
 
-# Costruisco il percorso assoluto ai file csv
+# Costruisco il percorso ai file csv
 stato_lavori_path = os.path.join(data_root, "stato_lavori.csv")
 italy_geo_path = os.path.join(data_root, "italy_geo.csv")
 pcn_route_path = os.path.join(data_root, "pcn_route.csv")
 
 
-# Imposto una funzione che mi ritorni le path dei file csv in un dizionario
 def load_config():
-    # print(stato_lavori_path)
     return {
         "italy_geo": italy_geo_path,
         "pcn_route": pcn_route_path,
